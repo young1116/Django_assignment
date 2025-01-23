7b632e0fcb809e672c2b4d44c1d9747b7a922ceb
leeyoungjoo <youngjoo6452@gmail.com>
2025-01-23 13:12:25 +0900

    Add URL patterns for Post CRUD operations

--- myproject/urls.py
+++ myproject/urls.py
@@ -3,5 +3,6 @@ from django.urls import include, path  # URL 설정 모듈
 
 urlpatterns = [
     path('admin/', admin.site.urls),  # 관리자 페이지 URL
-    path('user/', include('user.urls')),  # user 앱 URL 포함 
+    path('user/', include('user.urls')), #user 앱 URL 포함 
+    path('post/', include('post.urls')),  # post 앱 URL 포함  
 ]
