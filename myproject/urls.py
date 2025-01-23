249cb5e407ca8540197de98659d8df696ff228eb
leeyoungjoo <youngjoo6452@gmail.com>
2025-01-23 12:18:20 +0900

    Update myproject/urls.py

--- myproject/urls.py
+++ myproject/urls.py
@@ -1,8 +1,7 @@
 from django.contrib import admin  # 관리자 페이지
 from django.urls import include, path  # URL 설정 모듈
 
-
 urlpatterns = [
-    path('admin/', admin.site.urls),  #관리자 페이지 url
-    path('user/', include ('user.urls')), #user 앱 url포함 
+    path('admin/', admin.site.urls),  # 관리자 페이지 URL
+    path('user/', include('user.urls')),  # user 앱 URL 포함 
 ]
