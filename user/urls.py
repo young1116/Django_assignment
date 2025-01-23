f4bd91b19dc1e0869009b16a45e98d083d7af66a
leeyoungjoo <youngjoo6452@gmail.com>
2025-01-23 11:31:09 +0900

    Add urls.py

---
+++ user/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path #url 경로 설정 모듈듈
+from .views import SignUpView, LoginView, LogoutView #뷰 가져오기 
+
+urlpatterns = [
+    path('signup/', SignUpView.as_view(), name='signup'), #회원가입 url
+    path('login/', LoginView.as_view(), name='login'),    #로그인 url
+    path('logout/', LogoutView.as_view(), name='logout'), #로그아웃 url
+]
